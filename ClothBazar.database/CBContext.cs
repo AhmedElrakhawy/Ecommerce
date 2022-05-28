8f54fb328f6bdc8b3c5f01b2a4d5e36d40f0a26c
ahmed elrakhawy <elrakhawyahmed79@gmail.com>
2022-05-28 09:31:38 +0200

    AddingProductAndCategoryAddIntializeTheDatabse

---
+++ ClothBazar.database/CBContext.cs
@@ -0,0 +1,20 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.database
+{
+    public class CBContext : DbContext
+    {
+        public CBContext() : base("ClothBazarDefaultConection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
