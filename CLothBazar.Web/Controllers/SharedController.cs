53c33700166f40538224de03cfd9637f619bdec5
ahmed elrakhawy <elrakhawyahmed79@gmail.com>
2022-06-09 00:36:31 +0200

    Adding ImageUrl To Category Table And Solving Image Loading Problem

---
+++ CLothBazar.Web/Controllers/SharedController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class SharedController : Controller
+    {
+        // GET: Shared
+        public JsonResult UploadImage()
+        {
+            var Result = new JsonResult();
+            //Request.RequestContext.
+            try
+            {
+                var file = Request.Files[0];
+                var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Content/images/"), fileName);
+                file.SaveAs(path);
+                Result.Data = new
+                {
+                    Success = true,
+                    ImageURL = path
+                };
+            }
+            catch (Exception ex)
+            {
+                Result.Data = new { Success = false, Message = ex.Message };
+            }
+
+            return Result;
+        }
+    }
+}
---
+++ CLothBazar.Web/ViewModel/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ClothBazar.Entities;
+using System.Collections.Generic;
+
+namespace ClothBazar.Web.ViewModel
+{
+    public class HomeViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
