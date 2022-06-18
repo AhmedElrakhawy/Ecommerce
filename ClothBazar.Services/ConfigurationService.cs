7e94a6f9c71ca1868a48ed19c9ab63e3347f29cf
ahmed elrakhawy <elrakhawyahmed79@gmail.com>
2022-06-18 06:42:07 +0200

    Adding Custom URLs To The Project

---
+++ ClothBazar.Services/ConfigurationService.cs
@@ -0,0 +1,21 @@
+﻿using ClothBazar.database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+    public class ConfigurationService
+    {
+        public Config GetConfig(string Key)
+        {
+            using(var Context = new CBContext())
+            {
+                return Context.Configurations.Find(Key);
+            }
+        }
+    }
+}
---
+++ ClothBazar.database/Migrations/202206180119517_ChangingConfigurationsTableName.cs
@@ -0,0 +1,16 @@
+namespace ClothBazar.database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangingConfigurationsTableName : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
