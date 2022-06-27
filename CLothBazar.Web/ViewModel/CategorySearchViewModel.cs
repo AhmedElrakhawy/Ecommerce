5db8c5060b1eefd2a092dae79884d6d7a6c9a828
ahmed elrakhawy <elrakhawyahmed79@gmail.com>
2022-06-27 10:24:29 +0200

    Refactoring View Models

---
+++ CLothBazar.Web/ViewModel/CategorySearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothBazar.Web.ViewModel
+{
+    public class CategorySearchViewModel
+    {
+        public Pager Pager { get; set; }
+        public string SearchTerm { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
